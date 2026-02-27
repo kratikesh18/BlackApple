2f0074570911f5560113216d1edbc0663a962642
kratikesh18 <im.kartikesh18@gmail.com>
2026-02-27 19:57:40 +0530

    fixed profile/login middleware issue (Production bug)

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,18 +2,18 @@ import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware(request: NextRequest) {
-  console.log("\n========== 🔐 MIDDLEWARE START ==========");
+  // console.log("\n========== 🔐 MIDDLEWARE START ==========");
 
-  console.log("➡️ URL:", request.url);
+  // console.log("➡️ URL:", request.url);
 
-  console.log("➡️ Pathname:", request.nextUrl.pathname);
+  // console.log("➡️ Pathname:", request.nextUrl.pathname);
 
-  console.log("➡️ NEXTAUTH_SECRET exists:", !!process.env.NEXTAUTH_SECRET);
+  // console.log("➡️ NEXTAUTH_SECRET exists:", !!process.env.NEXTAUTH_SECRET);
 
-  console.log(
-    "➡️ NEXTAUTH_SECRET length:",
-    process.env.NEXTAUTH_SECRET?.length || 0,
-  );
+  // console.log(
+  //   "➡️ NEXTAUTH_SECRET length:",
+  //   process.env.NEXTAUTH_SECRET?.length || 0,
+  // );
 
   // Try getting token
   const token = await getToken({
@@ -21,38 +21,38 @@ export async function middleware(request: NextRequest) {
     secret: process.env.NEXTAUTH_SECRET,
   });
 
-  console.log("➡️ Token exists:", !!token);
+  // console.log("➡️ Token exists:", !!token);
 
-  console.log("➡️ Full Token:", token);
+  // console.log("➡️ Full Token:", token);
 
   const { pathname } = request.nextUrl;
 
-  // 🚫 NOT AUTHENTICATED
+  // NOT AUTHENTICATED
   if (!token && pathname.startsWith("/profile")) {
-    console.log("❌ User NOT authenticated");
+    // console.log(" User NOT authenticated");
 
-    console.log("🔁 Redirecting to /login");
+    // console.log("🔁 Redirecting to /login");
 
-    console.log("========== 🔐 MIDDLEWARE END ==========\n");
+    // console.log("========== 🔐 MIDDLEWARE END ==========\n");
 
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
-  // ✅ AUTHENTICATED but accessing login
+  //  AUTHENTICATED but accessing login
   if (token && pathname.startsWith("/login")) {
-    console.log("✅ User authenticated");
+    // console.log(" User authenticated");
 
-    console.log("🔁 Redirecting to /profile");
+    // console.log(" Redirecting to /profile");
 
-    console.log("========== 🔐 MIDDLEWARE END ==========\n");
+    // console.log("========== 🔐 MIDDLEWARE END ==========\n");
 
     return NextResponse.redirect(new URL("/profile", request.url));
   }
 
-  // ✅ Allow
-  console.log("✅ Access allowed");
+  //  Allow
+  // console.log("✅ Access allowed");
 
-  console.log("========== 🔐 MIDDLEWARE END ==========\n");
+  // console.log("========== 🔐 MIDDLEWARE END ==========\n");
 
   return NextResponse.next();
 }
